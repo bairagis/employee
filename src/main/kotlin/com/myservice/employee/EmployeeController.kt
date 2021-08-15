5fb8ede861f1c8640461193eb83c692af1089f4c
bairagis <sudip.bairagi@gmail.com>
2021-08-15 15:09:06 +0530

    init
    first commit

---
+++ src/main/kotlin/com/myservice/employee/EmployeeController.kt
@@ -0,0 +1,15 @@
+package com.myservice.employee
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("v1")
+class EmployeeController {
+    @GetMapping("/names")
+    fun getEmployeeNames():String {
+        var employeeList:List<String> = listOf("Sudip","Amit")
+        return employeeList.toString()
+    }
+}
